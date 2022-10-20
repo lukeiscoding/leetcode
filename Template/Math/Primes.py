09570c76c5dc0ab4cc23f867d9e9dfff86335f86
Shuyu Gong <13910413+lukeiscoding@users.noreply.github.com>
2022-10-19 23:20:53 -0400

    Update Primes.py

--- Template/Math/Primes.py
+++ Template/Math/Primes.py
@@ -3,12 +3,12 @@ def eratosthenes(n):
     q = [0] * (n + 1)
     primes = []
     for i in range(2, int(sqrt(n))):
-        if q[i] != 1:
+        if q[i] == 0:
             j = i * 2
-            while j <= n:
+            while j < n:
                 q[j] = 1
                 j += i
-    for i in range(2, n):
+    for i in range(2, n + 1):
         if q[i] == 0:
             primes.append(i)
     return primes
